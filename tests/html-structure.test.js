b60f16cc5a15481cceb01c2e132e1ccb718a6346
Viktor <shvetsviktor89@gmail.com>
2026-06-18 16:43:51 +0100

    add navbar and move hero to header.

--- tests/html-structure.test.js
+++ tests/html-structure.test.js
@@ -15,13 +15,16 @@ describe("Validate that elements are present on the page", () => {
     test("header is on the page", () => {
         expect(document.getElementsByTagName("header").length).toBe(1);
     });
+    test("navbar is on the page", () => {
+        expect(document.querySelectorAll("header nav").length).toBe(1);
+    });
     test("hero is on the page", () => {
       expect(document.querySelectorAll("section#hero").length).toBe(1);
     })
     test("services is on the page", () => {
       expect(document.querySelectorAll("section#services").length).toBe(1);
     })
-    test("services is on the page", () => {
+    test("portfolio is on the page", () => {
       expect(document.querySelectorAll("section#portfolio").length).toBe(1);
     })
     test("howItWorks is on the page", () => {
