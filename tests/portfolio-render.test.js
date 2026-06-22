9e09c645917ab12b7aa726ee44b2cf798d8ebb79
Viktor <shvetsviktor89@gmail.com>
2026-06-22 23:39:26 +0100

    test(portfolio): refactor Portfolio rendering test

--- tests/portfolio-render.test.js
+++ tests/portfolio-render.test.js
@@ -1,3 +1,5 @@
+const { renderPortfolioSlides } = require('../assets/js/portfolio-carousel');
+
 describe('Portfolio rendering', () => {
   beforeEach(() => {
     document.body.innerHTML = `
@@ -7,20 +9,8 @@ describe('Portfolio rendering', () => {
     `;
   });
 
-  test('renders one portfolio slide from data', () => {
-    const items = [
-      {
-        id: 'balloon-arches-1',
-        title: 'Pastel Balloon Arch',
-        category: 'balloon-arches',
-        image: 'assets/images/portfolio/balloon-arches-1.avif',
-        alt: 'Pastel balloon arch decoration for a celebration',
-        description: 'A soft pastel balloon arch created for a private celebration.',
-        price: 120,
-      },
-    ];
-
-    renderPortfolioSlides(items);
+  test('renders one portfolio slide', () => {
+    renderPortfolioSlides([{}]);
 
     expect(document.querySelectorAll('.portfolio-card.swiper-slide').length).toBe(1);
   });
