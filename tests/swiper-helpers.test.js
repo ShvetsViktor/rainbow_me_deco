f027db826b0b732b8608cc14a6ff0b9784aa4b06
Viktor <shvetsviktor89@gmail.com>
2026-06-25 13:13:45 +0100

    test(swiper): remove pagination from tabindex

---
+++ tests/swiper-helpers.test.js
@@ -0,0 +1,22 @@
+describe('Swiper helpers', () => {
+  test('removes pagination bullets from keyboard tab order', () => {
+    document.body.innerHTML = `
+      <div class="swiper">
+        <div class="swiper-pagination">
+          <span class="swiper-pagination-bullet" tabindex="0"></span>
+          <span class="swiper-pagination-bullet" tabindex="0"></span>
+        </div>
+      </div>
+    `;
+
+    const swiperElement = document.querySelector('.swiper');
+
+    removePaginationBulletsFromTabOrder(swiperElement);
+
+    const paginationBullets = document.querySelectorAll('.swiper-pagination-bullet');
+
+    paginationBullets.forEach((bullet) => {
+      expect(bullet.getAttribute('tabindex')).toBe('-1');
+    });
+  });
+});
