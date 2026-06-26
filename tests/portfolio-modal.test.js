6c381035203d9973bb364f9a1bbd503b114939f6
Viktor <shvetsviktor89@gmail.com>
2026-06-26 11:47:53 +0100

    test(portfolio-modal): create test for initPortfolioImageModal

---
+++ tests/portfolio-modal.test.js
@@ -0,0 +1,39 @@
+const { renderPortfolioSlides } = require('../assets/js/portfolio-carousel');
+
+const testPortfolioItems = [
+  {
+    title: 'Pastel Balloon Arch',
+    category: 'balloon-arches',
+    image: 'assets/images/portfolio/balloon-arches-1.avif',
+    alt: 'Pastel balloon arch decoration',
+    description: 'A soft pastel balloon arch.',
+    price: 120,
+  },
+];
+
+test('opens portfolio image modal when portfolio image button is clicked', () => {
+  document.body.innerHTML = `
+    <div class="swiper portfolio-swiper">
+      <div class="swiper-wrapper"></div>
+    </div>
+
+    <div class="portfolio-modal" hidden>
+      <button class="portfolio-modal-close" type="button">Close</button>
+      <img class="portfolio-modal-image" src="" alt="" />
+    </div>
+  `;
+
+  renderPortfolioSlides(testPortfolioItems);
+  initPortfolioImageModal();
+
+  const imageButton = document.querySelector('.portfolio-image-button');
+
+  imageButton.click();
+
+  const modal = document.querySelector('.portfolio-modal');
+  const modalImage = document.querySelector('.portfolio-modal-image');
+
+  expect(modal.hidden).toBe(false);
+  expect(modalImage.getAttribute('src')).toBe(testPortfolioItems[0].image);
+  expect(modalImage.getAttribute('alt')).toBe(testPortfolioItems[0].alt);
+});
