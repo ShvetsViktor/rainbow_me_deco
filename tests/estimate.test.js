880a516a70b8eddbeb0714e30a39f237b5b5fef5
Viktor <shvetsviktor89@gmail.com>
2026-06-26 12:57:51 +0100

    test(est): check that addItemToEstimate returns correct data

---
+++ tests/estimate.test.js
@@ -0,0 +1,15 @@
+const { addItemToEstimate } = require('../assets/js/estimate');
+
+const testItem = {
+  title: 'Pastel Balloon Arch',
+  category: 'balloon-arches',
+  price: 120,
+};
+
+test('adds item to estimate list', () => {
+  const estimateItems = [];
+
+  const updatedEstimateItems = addItemToEstimate(estimateItems, testItem);
+
+  expect(updatedEstimateItems).toEqual([testItem]);
+});
